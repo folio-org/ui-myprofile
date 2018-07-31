4463d95c7a4c045f335ebc6bee197d3ca7987b5e
Viktor Soroka <Victor_Soroka1@epam.com>
2018-07-31 13:00:09 +0300

    UIMPROF-3: Restrict access to Change Password page if user logged in under SSO.

--- settings/index.js
+++ settings/index.js
@@ -1,29 +1,51 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Settings from '@folio/stripes-components/lib/Settings';
-import { stripesShape } from '@folio/stripes-core/src/Stripes'; // eslint-disable-line import/no-unresolved
 
 import ChangePassword from './ChangePassword';
 
-class MyProfile extends Component { // eslint-disable-line react/no-deprecated
+class MyProfile extends Component {
   static propTypes = {
-    stripes: stripesShape.isRequired,
-    resources: PropTypes.shape({
-      creds: PropTypes.object
+    stripes: PropTypes.shape({
+      okapi: PropTypes.shape({
+        ssoEnabled: PropTypes.bool,
+      }).isRequired,
+      intl: PropTypes.shape({
+        formatMessage: PropTypes.func.isRequired
+      }).isRequired
     }).isRequired,
   };
 
   constructor(props) {
     super(props);
 
-    this.pages = [
-      {
-        route: 'password',
-        label: this.props.stripes.intl.formatMessage({ id: 'ui-myprofile.settings.changePassword.label' }),
-        component: ChangePassword,
-        perm: 'ui-myprofile.view',
-      },
-    ];
+    this.pages = this.createSettingsPages();
+  }
+
+  createSettingsPages() {
+    return []
+      .concat(
+        this.registerChangePasswordPage()
+      )
+      .filter((page) => page);
+  }
+
+  registerChangePasswordPage() {
+    const { stripes: { intl: { formatMessage }, okapi: { ssoEnabled } } } = this.props;
+    const showPasswordPage = !ssoEnabled;
+
+    if (!showPasswordPage) {
+      return undefined;
+    }
+
+    const changePasswordPageSettings = {
+      route: 'password',
+      label: formatMessage({ id: 'ui-myprofile.settings.changePassword.label' }),
+      component: ChangePassword,
+      perm: 'ui-myprofile.view',
+    };
+
+    return changePasswordPageSettings;
   }
 
   render() {
