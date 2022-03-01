b33001e2959afcfd6020f03b5c8fe3a023f2b007
Stanislau Snapok <stanislau_snapok@epam.com>
2022-03-01 14:39:10 +0300

    tests fix

--- test/jest/babel.config.js
+++ test/jest/babel.config.js
@@ -1,11 +1,17 @@
+const { babelOptions } = require('@folio/stripes-cli');
+
+babelOptions.plugins.push([
+  'babel-plugin-module-resolver',
+  {
+    root: ['./'],
+    alias: {
+      __mock__: './test/jest/__mock__',
+      fixtures: './test/jest/fixtures',
+      helpers: './test/jest/helpers',
+    },
+  },
+]);
+
 module.exports = {
-  'presets': [
-    '@babel/preset-env',
-    '@babel/preset-react',
-  ],
-  'plugins': [
-    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
-    ['@babel/plugin-proposal-class-properties', { 'loose': true }],
-    '@babel/plugin-transform-runtime',
-  ],
+  ...babelOptions,
 };
