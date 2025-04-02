3e4f87bbacb06565eea5fce69540146313df0c8d
John Coburn <jcoburn@ebsco.com>
2025-04-02 14:55:23 -0500

    set up sortable items with drag handles

--- src/settings/AppOrderSettings/components/DraggableAppOrderItem.js
+++ src/settings/AppOrderSettings/components/DraggableAppOrderItem.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import { useIntl } from 'react-intl';
-import { Icon } from '@folio/stripes/components';
+import uniqueId from 'lodash/uniqueId';
+import { IconButton } from '@folio/stripes/components';
 import listCss from './AppOrderList.css';
 
 // Item template for the draggable nav-reorder list.
@@ -16,7 +17,7 @@ export default ({ id, children, index, isNew }) => { // eslint-disable-line reac
     transform,
     transition,
   } = useSortable({ id });
-
+  const itemId = useRef(uniqueId('draggable-label-')).current;
   const style = {
     transform: CSS.Transform.toString(transform),
     transition
@@ -24,14 +25,14 @@ export default ({ id, children, index, isNew }) => { // eslint-disable-line reac
 
   return (
     <li ref={setNodeRef} className={`${listCss.reorderListItem}`} style={style}>
-      <div data-draggable-item={id} className={`${listCss.listItemContent}${active?.id === id ? ' ' + listCss.activeItem : ''}`} {...attributes} {...listeners}>
-        <div className={listCss.listItemLabel}>
+      <div data-draggable-item={id} className={`${listCss.listItemContent}${active?.id === id ? ' ' + listCss.activeItem : ''}`}>
+        <div className={listCss.listItemLabel} id={itemId}>
           <span>{index}.</span>
           {children}
         </div>
         <div>
           {isNew && <strong>{formatMessage({ id: 'ui-myprofile.settings.appNavOrder.newItem' })}</strong> }
-          <Icon iconClassName={listCss.dragIcon} icon="drag-drop" style={{ opacity: '50' }} />
+          <IconButton {...attributes} {...listeners} aria-labelledby={itemId} iconClassName={listCss.dragIcon} icon="drag-drop" style={{ opacity: '50' }} />
         </div>
       </div>
     </li>
