66fd6b1335417df0dbcd6c3cb02ca88b4e3378a0
Dmytro Melnyshyn <dmytro_melnyshyn@epam.com>
2026-09-23 20:04:02 +0000

    UIMPROF-127: Settings > My profile > Language & display > change component
    Task:
    UIMPROF-127: Settings > My profile > Language & display > change component
    
    Acceptance criteria:
    Replace the current Language selection dropdown with the FOLIO Stripes selection component documented at https://dev.folio.org/stripes-components/?path=/docs/ux-guidelines-forms-selection--docs. Configure its search behavior to use contains matching.
    
    FAIF Flow D execution 3aee5b04-586b-4e87-b669-de3342b25d69

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const config = require('@folio/jest-config-stripes');
+const { config } = require('@folio/jest-config-stripes');
 
 module.exports = {
   ...config,
@@ -7,10 +7,4 @@ module.exports = {
     ...config.setupFiles,
     path.join(__dirname, './test/jest/setupFiles.js'),
   ],
-  // keyboardjs ships an untranspiled ESM source that @folio/stripes-components'
-  // HotKeys imports directly; without this it fails with a syntax error under jest.
-  transformIgnorePatterns: config.transformIgnorePatterns.map((pattern) => pattern.replace(
-    '(?!@folio',
-    '(?!keyboardjs|@folio',
-  )),
 };
